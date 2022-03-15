42ecaff533bb5e59e72e2c74bcec91e7e7d08cc5
Kevin <hellokevinvogel@gmail.com>
2022-03-15 11:54:09 +0100

    chore(): create file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
