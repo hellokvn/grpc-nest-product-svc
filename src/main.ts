601772598c009a7abcea19a8a83d01198612e5a5
Kevin <hellokevinvogel@gmail.com>
2022-03-15 12:14:23 +0100

    chore(): create service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { INestMicroservice, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { protobufPackage } from './product/product.pb';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestMicroservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50052',
+      package: protobufPackage,
+      protoPath: join('node_modules/grpc-nest-proto/proto/product.proto'),
+    },
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen();
 }
+
 bootstrap();
