83416b97bde61f08c0d7c7479dbf362aaca0294a
Kevin <hellokevinvogel@gmail.com>
2022-03-15 12:08:54 +0100

    chore(): add service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+import { CreateRequestDto, FindOneRequestDto } from './product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  @InjectRepository(Product)
+  private readonly repository: Repository<Product>;
+
+  public findOne({ id }: FindOneRequestDto): Promise<Product> {
+    return this.repository.findOne(id);
+  }
+
+  public create(payload: CreateRequestDto): Promise<Product> {
+    const product: Product = new Product();
+
+    product.name = payload.name;
+    product.sku = payload.sku;
+    product.stock = payload.stock;
+    product.price = payload.price;
+
+    return this.repository.save(product);
+  }
+}
