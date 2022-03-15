1820ef29ca6be95cbefe3de99858234f9e1536fe
Kevin <hellokevinvogel@gmail.com>
2022-03-15 12:14:06 +0100

    chore(): add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'micro_product',
+      username: 'kevin',
+      password: null,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
+import { Product } from './product.entity';
 import { ProductService } from './product.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
   providers: [ProductService],
 })
