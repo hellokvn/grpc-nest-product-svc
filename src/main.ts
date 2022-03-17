742259370c6b93163cc86d17253f8545259850cf
Kevin <hellokevinvogel@gmail.com>
2022-03-17 12:33:23 +0100

    chore(): rm redundant

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { INestMicroservice, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { protobufPackage } from './product/product.pb';
 
 async function bootstrap() {
-  const app: INestMicroservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const app: INestMicroservice = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
       url: '0.0.0.0:50053',
